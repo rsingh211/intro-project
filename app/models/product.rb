632d25fe0acf75a1416e0d604cc47bb2c0f4a18b
rsingh211 <rsingh15@academic.rrc.ca>
2025-03-20 03:16:09 -0500

    Added model validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_many :products
     has_and_belongs_to_many :star_wars_characters
+    validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
   end
   
